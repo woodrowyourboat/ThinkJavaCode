58993ec095b15834e81c857e3171f9e4911068be
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-18 15:51:07 -0600

    First build

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,12 @@
+public class DoubleByZero
+{
+    public static void main(String args[])
+    {
+        double v1 = 42;
+        double v2 = 0;
+        double result = (v1 / v2);
+
+        System.out.println(result);
+
+    }
+}
