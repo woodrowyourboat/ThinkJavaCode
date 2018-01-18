c4f051b2c52e4c0d1965868ab4c93e6df688388f
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-18 15:42:46 -0600

    First build

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,28 @@
+public class IntExtremes
+{
+    public static void main(String args[])
+    {
+        int positiveInt = 2147483647;
+
+        System.out.println(positiveInt);
+
+
+        positiveInt = (positiveInt + 1);
+
+        System.out.println(positiveInt);
+
+
+
+        int negativeInt = -2147483648;
+
+        System.out.println(negativeInt);
+
+
+        negativeInt = (negativeInt - 1);
+
+        System.out.println(negativeInt);
+
+
+    }
+
+}
