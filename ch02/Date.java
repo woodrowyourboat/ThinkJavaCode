5de989164d1b212124b1353158c5e6f961404e0d
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-18 14:26:33 -0600

    First build

---
+++ ch02/Date.java
@@ -0,0 +1,46 @@
+public class Date
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello world!");
+
+
+        int hour = 2;
+        int minute = 21;
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+
+        String day = "Thursday";
+        String month = "January";
+        int date = 18;
+        int year = 2018;
+
+        System.out.println("American format:");
+        System.out.print("Today's date is ");
+        System.out.print(day + ", ");
+        System.out.print(month + " ");
+        System.out.print(date + ", ");
+        System.out.println(year + ".");
+
+        System.out.println("European format:");
+        System.out.print("Today's date is ");
+        System.out.print(day + " ");
+        System.out.print(date + " ");
+        System.out.print(month + " ");
+        System.out.println(year + ".");
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ ch02/MyVariables.java
@@ -0,0 +1,21 @@
+public class MyVariables
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hi there!");
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+
+        int d = 5;
+        int e = 2;
+        int f = d - e;
+        System.out.println(f);
+
+
+
+    }
+
+}
