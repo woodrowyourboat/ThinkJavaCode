84e1e4ea85cc8040f4c44f3f23313791bd850e98
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-18 15:47:23 -0600

    First build

---
+++ ch02/IntByZero.java
@@ -0,0 +1,14 @@
+public class IntByZero
+{
+    public static void main(String args[])
+    {
+        int v1 = 42;
+        int v2 = 0;
+        int result = (v1 / v2);
+
+        System.out.println(result);
+
+
+    }
+}
+
