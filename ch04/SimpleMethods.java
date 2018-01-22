fe51fd03b2e98b3587f056a10a3cc9473aede05a
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-22 08:30:56 -0600

    First run of 4.2

---
+++ ch04/SimpleMethods.java
@@ -0,0 +1,13 @@
+public class SimpleMethods
+{
+    public static void main(String args[])
+    {
+        printCount(5);
+
+    }
+
+    public static void printCount(int count)
+    {
+        System.out.print("This count is: " + count);
+    }
+}
