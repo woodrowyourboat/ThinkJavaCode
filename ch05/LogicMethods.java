ee749f933601324f64a8398bb81863fc67a9cd24
woodrowyourboat <woodrowliddell@gmail.com>
2018-01-22 11:31:29 -0600

    5-C finished. Added notes to help aid reading code.

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -2,6 +2,9 @@ public class LogicMethods
 {
     public static void main(String args[])
     {
+        //Exercises 5A and 5B output begin here:
+        System.out.println("Exercise 5-A and B \n");
+
         printIsLarge(4);
         printIsLargeOrSmall(4);
 
@@ -10,10 +13,18 @@ public class LogicMethods
 
         printIsLarge(101);
         printIsLargeOrSmall(101);
+        //5A and 5B outputs end here.
 
 
+        //5C output begins here:
+        System.out.println("\n Exercise 5-C:\n");
+
+        printLargest(22, 37);
+        printLargest(21, 21);
+        //5C output ends here.
     }
 
+    //5A and 5B methods begin here:
     private static void printIsLarge(int number)
     {
         System.out.println("Number is: " + number);
@@ -27,14 +38,31 @@ public class LogicMethods
 
     private static void printIsLargeOrSmall(int number)
     {
+
         if (number > 99)
         {
-            System.out.println("The number is large\n");
+            System.out.println("The number is large.\n");
         }
         else if (number < 10)
         {
-            System.out.println("The number is small\n");
+            System.out.println("The number is small.\n");
         }
     }
+    //5A and 5B methods end here.
 
+    //5C methods begin here:
+    private static void printLargest(int number1, int number2)
+    {
+        System.out.println("The numbers are: " + number1 + " and " + number2);
+
+        if (number1 != number2)
+        {
+            System.out.println("The largest number is: " + Math.max(number1, number2) + "\n");
+        }
+        else
+        {
+            System.out.println("The numbers are equal.\n");
+        }
+    }
+    //5C methods end here.
 }
